aa4e09f89b6f50d7d81034b06295eb8f68ef1529
abeni <abeni@email.com>
2025-12-17 14:48:00 +0300

    add gallery page

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,25 @@
+import "./Gallery.css";
+
+import img1 from "../assets/images/p1.jpg";
+import img2 from "../assets/images/p2.jpg";
+import img3 from "../assets/images/p3.jpg";
+import img4 from "../assets/images/p4.jpg";
+import img6 from "../assets/images/p6.jpg";
+
+
 const Gallery = () => {
   return (
-    <div>
-      <h1>My Work</h1>
-      <p>Here are some of my favorite shots.</p>
-    </div>
+    <section className="gallery">
+      <h1>Gallery</h1>
+
+      <div className="gallery-grid">
+        <img src={img1} alt="Gallery 1" />
+        <img src={img2} alt="Gallery 2" />
+        <img src={img3} alt="Gallery 3" />
+        <img src={img4} alt="Gallery 4" />
+        <img src={img6} alt="Gallery 6" />
+      </div>
+    </section>
   );
 };
 
